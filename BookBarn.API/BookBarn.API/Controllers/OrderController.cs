5506d6d2697774501b5ef8ae68a7be75cb4cc766
Sudeep A <sudeepma99@gmail.com>
2024-04-18 17:43:47 +0530

    chnaged the api

--- BookBarn.API/BookBarn.API/Controllers/OrderController.cs
+++ BookBarn.API/BookBarn.API/Controllers/OrderController.cs
@@ -1,14 +1,19 @@
-﻿using System;
+﻿using BookBarn.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.OData;
 
 namespace BookBarn.API.Controllers
 {
     public class OrderController : ApiController
     {
+
+
+
         // get
         // api/order/
         // get all orders (only for the admin) 
@@ -29,8 +34,21 @@ namespace BookBarn.API.Controllers
         // api/order/{id}
         // edit the status for admin to change the status
         [HttpPatch]
-        public IHttpActionResult PatchEditStatus(int id, [])
+        public IHttpActionResult PatchEditStatus(int id, [FromBody] Delta<Order>order)
         {
+
+
+            if(order == null)
+            {
+                return BadRequest("Missing data to patch");
+            }
+
+            var existingOrder = new Order();
+            order.Patch(existingOrder);
+            
+            //DB.save changes
+
+
             return Ok();
         }
     }
