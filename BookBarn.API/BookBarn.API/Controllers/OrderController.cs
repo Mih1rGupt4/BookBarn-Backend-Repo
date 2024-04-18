11278f48111eb20cdc12ff28a06aba217032eae6
Sudeep A <sudeepma99@gmail.com>
2024-04-18 15:43:03 +0530

    added comments about api  functions

--- BookBarn.API/BookBarn.API/Controllers/OrderController.cs
+++ BookBarn.API/BookBarn.API/Controllers/OrderController.cs
@@ -9,5 +9,29 @@ namespace BookBarn.API.Controllers
 {
     public class OrderController : ApiController
     {
+        // get
+        // api/order/
+        // get all orders (only for the admin) 
+
+
+        // get
+        // api/order/{id}
+        // get all orders by user
+
+
+        // post
+        // api/order
+        // to post the order
+
+
+        // patch
+        // api/order/cancel/{id}
+        // edit the status for user to cancel
+ 
+
+        // patch
+        // api/order/{id}
+        // edit the status for admin to change the status
+
     }
 }
