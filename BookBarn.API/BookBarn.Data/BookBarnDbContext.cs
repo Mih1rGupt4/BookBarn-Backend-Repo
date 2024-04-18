c0a086b512d8b2fa4705512f74fbc708efc6f288
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-04-18 14:42:59 +0530

    add db context

---
+++ BookBarn.API/BookBarn.Data/BookBarnDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Data
+{
+    public class BookBarnDbContext : DbContext
+    {
+        public BookBarnDbContext() : base("")
+        {
+            
+        }
+
+
+    }
+}
