d56d0c89902737bc695c40b81f7526cebd00be53
Sudeep A <sudeepma99@gmail.com>
2024-04-18 15:18:41 +0530

    added Address, PaymentDetails, OrderStatus

---
+++ BookBarn.API/BookBarn.Domain/Entities/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace BookBarn.Domain.Entities
+{
+    public enum OrderStatus
+    {
+        Ordered,
+        Cancelled,
+        Packed,
+        Dispatched,
+        OnTheWay,
+        Delivered,
+        ReturnRequested,
+        ReplacedRequested,
+        ReturnScheduled,
+        ReturnCompleted,
+        ReplacedCompleted
+    }
+
+}
