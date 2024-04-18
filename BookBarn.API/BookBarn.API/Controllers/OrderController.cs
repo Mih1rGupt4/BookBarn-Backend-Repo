833b48af1eb87220ba68a0e901885a6592f7bdf2
Jeferson X <86565887+accelrock14@users.noreply.github.com>
2024-04-18 17:50:32 +0530

    getorder api function added

--- BookBarn.API/BookBarn.API/Controllers/OrderController.cs
+++ BookBarn.API/BookBarn.API/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookBarn.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -12,11 +13,35 @@ namespace BookBarn.API.Controllers
         // get
         // api/order/
         // get all orders (only for the admin) 
+        public IHttpActionResult GetProductById()
+        {
+            var orders = new List<Order>();
+            if (orders == null)
+            {
+                // not found
+                // return http status code 404
+                return NotFound();
+            }
+
+            return Ok(orders); // if found then return 200 with data
+        }
 
 
         // get
         // api/order/{id}
         // get all orders by user
+        public IHttpActionResult GetProductById(int id)
+        {
+            var order = new Order();
+            if (order == null)
+            {
+                // not found
+                // return http status code 404
+                return NotFound();
+            }
+
+            return Ok(order); // if found then return 200 with data
+        }
 
 
         // post
@@ -29,7 +54,7 @@ namespace BookBarn.API.Controllers
         // api/order/{id}
         // edit the status for admin to change the status
         [HttpPatch]
-        public IHttpActionResult PatchEditStatus(int id, [])
+        public IHttpActionResult PatchEditStatus(int id)
         {
             return Ok();
         }
