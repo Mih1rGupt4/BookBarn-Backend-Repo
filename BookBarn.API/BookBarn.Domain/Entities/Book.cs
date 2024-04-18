955ec199bbd4bb587da8114794b7d535cad85e55
Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
2024-04-18 15:09:25 +0530

    book-entity-class

---
+++ BookBarn.API/BookBarn.Domain/Entities/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Domain.Entities
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string YearOfPublication { get; set; }
+        public string ImageUrlSmall { get; set; }
+        public string ImageUrlMedium { get; set; }
+        public string ImageUrlLarge { get; set;}
+
+    }
+}
