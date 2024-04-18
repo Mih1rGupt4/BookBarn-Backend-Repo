47921961da1707e6dacb5c3a38c47c8e0ad08565
S Soujanya <S Soujanya@DESKTOP-0LIF7NT>
2024-04-18 17:04:16 +0530

    updated Order

--- BookBarn.API/BookBarn.Domain/Entities/Order.cs
+++ BookBarn.API/BookBarn.Domain/Entities/Order.cs
@@ -20,7 +20,7 @@ namespace BookBarn.Domain.Entities
        
         public Address ShippingAddress { get; set; }
 
-        public string PaymentMethod {  get; set; }
+        public PaymentDetails PaymentMethod {  get; set; }
 
     }
 }
