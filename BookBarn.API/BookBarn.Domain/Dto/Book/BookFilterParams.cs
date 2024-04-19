4287ea767c09e64ac3fea0b38282bc9254f10aba
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-04-19 12:17:04 +0530

    book api creation

---
+++ BookBarn.API/BookBarn.Domain/Dto/Book/BookFilterParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Domain.Dto.Book
+{
+    public class BookFilterParams
+    {
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+    }
+
+}
