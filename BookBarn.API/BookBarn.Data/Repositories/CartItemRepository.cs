527b97841af1e509cc8f4589da8bc1edb1a38e1d
Sanjana <sanjana2002251@gmail.com>
2024-04-19 15:22:44 +0530

    Added Cart Item Repo

---
+++ BookBarn.API/BookBarn.Data/Repositories/CartItemRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookBarn.Domain.Entities;
+using BookBarn.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Data.Repositories
+{
+    public class CartItemRepository : ICartItemRepository
+    {
+        private BookBarnDbContext db;
+        CartItemRepository(BookBarnDbContext dbContext)
+        {
+            db  = dbContext;
+        }
+
+        public CartItem GetCartItemById(int itemId)
+        {
+            return db.CartItems.Find(itemId);
+
+        }
+
+        public void UpdateCartItem(CartItem item)
+        {
+           
+        }
+    }
+}
