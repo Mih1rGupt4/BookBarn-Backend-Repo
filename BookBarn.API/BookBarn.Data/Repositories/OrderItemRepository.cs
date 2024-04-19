5b7a905f8be072c3bea8197192bd1d04f95fa665
Soujanya-S6 <soujanya6620@gmail.com>
2024-04-19 14:40:46 +0530

    added OrderRepo and OrderItemRepo

---
+++ BookBarn.API/BookBarn.Data/Repositories/OrderItemRepository.cs
@@ -0,0 +1,23 @@
+﻿using BookBarn.Domain.Entities;
+using BookBarn.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Data.Repositories
+{
+    public class OrderItemRepository : IOrderItemRepository
+    {
+        private BookBarnDbContext db;
+        OrderItemRepository(BookBarnDbContext dbContext)
+        {
+            db = dbContext;
+        }
+        public OrderItem GetOrderItemById(int itemId)
+        {
+            return db.OrderItems.Where(orditem => orditem.OrderItemID == itemId).FirstOrDefault();
+        }
+    }
+}
