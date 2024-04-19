2bbf4909b9de40fc6364e1f9959084899c3332ee
Sanjana <sanjana2002251@gmail.com>
2024-04-19 10:08:15 +0530

    Added IOrderItemRepo and IOrderRepo

---
+++ BookBarn.API/BookBarn.Domain/Interfaces/IOrderItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Domain.Interfaces
+{
+    public interface IOrderItemRepository
+    {
+        OrderItem GetOrderItemById(int itemId);
+
+    }
+}
---
+++ BookBarn.API/BookBarn.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookBarn.Domain.Entities;
+
+
+namespace BookBarn.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Order GetOrderById(int orderId);
+        void AddOrder(int orderId, OrderItem order);
+        void UpdateOrder(int orderId,OrderItem order);
+        void RemoveOrder(int orderId, int ItemId);
+    }
+
+}
