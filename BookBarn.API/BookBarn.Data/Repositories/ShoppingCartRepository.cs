e3b0a6416040edad5d950d29139b7ceb822fb7f1
Sanjana <sanjana2002251@gmail.com>
2024-04-19 15:00:50 +0530

    Adding ShoppingCartRepository

---
+++ BookBarn.API/BookBarn.Data/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookBarn.Domain.Entities;
+using BookBarn.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Data.Repositories
+{
+    public class ShoppingCartRepository : IShoppingCartRepository
+    {
+        private List<CartItem> items;
+
+        public ShoppingCartRepository()
+        {
+            items = new List<CartItem>();
+        }
+        public void AddCartItem(int cartId, CartItem item)
+        {
+            
+        }
+
+        public ShoppingCart GetShoppingCartById(int cartId)
+        {
+            
+        }
+
+        public void RemoveCartItem(int cartId, int itemId)
+        {
+            
+        }
+    }
+}
