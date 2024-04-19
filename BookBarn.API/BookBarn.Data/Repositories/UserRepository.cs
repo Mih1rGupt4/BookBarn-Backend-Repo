28c28e02cf8c17e10fb353b9b2bfedb2042f80b2
sudhanvaB <sudhbalu@gmail.com>
2024-04-19 15:10:24 +0530

    User Authentication file structure updated

---
+++ BookBarn.API/BookBarn.Data/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookBarn.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+    }
+}
