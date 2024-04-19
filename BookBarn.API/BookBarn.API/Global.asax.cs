6f2fe0dbf08dfb75cbfbfcd270649cc68a530f89
Jeferson X <86565887+accelrock14@users.noreply.github.com>
2024-04-19 18:24:49 +0530

    enabled CORS to use api from frontend

--- BookBarn.API/BookBarn.API/Global.asax.cs
+++ BookBarn.API/BookBarn.API/Global.asax.cs
@@ -13,6 +13,8 @@ namespace BookBarn.API
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
             AreaRegistration.RegisterAllAreas();
             UnityConfig.RegisterComponents();
             GlobalConfiguration.Configure(WebApiConfig.Register);
