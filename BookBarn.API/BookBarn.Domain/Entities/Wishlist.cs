5b2ee95432aaab2566d26c41738c70626d5d2812
akjha-17 <amankumarj17@gmail.com>
2024-04-19 16:39:25 +0530

    adding wishlist

---
+++ BookBarn.API/BookBarn.Domain/Entities/Wishlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Domain.Entities
+{
+    public class Wishlist
+    {
+        [Key]
+        public int WishlistId { get; set; } 
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public virtual Book Book { get; set; }  
+    }
+}
---
+++ BookBarn.API/BookBarn.Domain/Interfaces/IWishlistRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBarn.Domain.Interfaces
+{
+    public interface IWishlist
+    {
+        
+        public List<Wishlist> GetWishlistItems(int userId);
+        void AddToWishlist(int userId, [FromBody] Book book)
+        void RemoveFromWishlist(int userId, int bookId);
+
+    }
+}
